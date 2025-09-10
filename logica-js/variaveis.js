588c66eea52de010f676dc36bed75fe1714d2f08
claro2025 <marcos.acoes89@gmail.com>
2025-09-09 22:14:30 -0300

    variaveis em java script

---
+++ logica-js/variaveis.js
@@ -0,0 +1,22 @@
+// variavel com var
+// var nome = "Marcos" 
+// var nome = "joão"
+// nome = "joão"
+
+//varialvel com let não aceita declarar duas variável com o mesmo nome, porém pode mudar o valor
+// let nome = "Marcos" 
+// nome = "João"
+
+// varialvel declarada com const
+const nome = "Marcos" //string
+const sobrenome = "Soares"
+const idade = 36 //number
+const masculino = true //boollean
+
+//array
+const frutas = ["laranja","maçã","banana"]
+
+console.log(frutas[0]) 
+
+//console.log("meu nome é " + nome + " " + sobrenome + " e minha idade é "+ idade + " anos" )
+//console.log(`meu nome é ${ nome} ${ sobrenome} e minha idade é ${ idade} anos`) 
