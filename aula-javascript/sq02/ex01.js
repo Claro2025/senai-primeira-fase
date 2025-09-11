c1e97c0e6fcb29a0391685621f71d6cc69d9ffee
claro2025 <marcos.acoes89@gmail.com>
2025-09-10 22:20:49 -0300

    aula-javascript

---
+++ aula-javascript/sq02/ex01.js
@@ -0,0 +1,21 @@
+// //questão 1
+let idade
+
+idade = Number( prompt(" Digite sua idade: ") )
+
+//idade = idade + 1  // ver como soma não string / concatenação
+
+
+if(idade >= 18){
+    alert("Maior de idade")
+    alert("Pode sair pro intervalo")
+}else{
+   alert("dimenó")
+   alert("pode sair pro recreio")
+}
+
+
+
+console.log(idade);
+
+
---
+++ aula-javascript/sq02/ex04.js
@@ -0,0 +1,13 @@
+let numero
+numero = Number( prompt(" Digite sua numero: ") )
+
+if(numero % 2 == 0){
+    alert("par")
+}else{
+   alert("impar")
+}
+
+console.log(numero);
+
+
+
